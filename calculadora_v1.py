1b5ff57ab3dce73832378e616d349d8bb90c9669
Eliabe Silva <119972073+eliabemsilva@users.noreply.github.com>
2023-02-13 08:32:18 -0300

    Add files via upload

--- calculadora_v1.py
+++ calculadora_v1.py
@@ -1,10 +1,7 @@
-# Calculadora em Python
-
-# Desenvolva uma calculadora em Python com tudo que você aprendeu nos capítulos até aqui no curso. 
-# A solução será apresentada no próximo capítulo!
-
+# Título da aplicação:
 print("\n******************* Calculadora em Python *******************")
 
+# Cria funções anônimas, com a expressão lamda:
 adicao = lambda x,y: x + y
 subtracao = lambda x,y: x - y
 multiplicacao = lambda x,y: x * y
@@ -12,6 +9,7 @@ divisao = lambda x,y: x / y
 potenciacao = lambda x,y: x ** y
 porcentagem = lambda x,y: (x * y) / 100
 
+# Apresenta os tipos de operações disponíveis na aplicação:
 print("\nOperações: \n")
 print("1 - Adição")
 print("2 - Subtração")
@@ -20,8 +18,11 @@ print("4 - Divisão")
 print("5 - Potenciação")
 print("6 - Porcentagem")
 
+# Apresenta uma caixa de texto para escolher a operação:
 escolha = int(input("\nDigite a sua opção (1, 2, 3, 4, 5 ou 6): "))
 
+# Condicionais para apresentar quais valores serão utilizados no cálculo,
+# Caso o usuário digita um número que não seja de 1 a 6, será apresentado o texto "Opção Inválida"
 if escolha >= 1 and escolha <= 6:
   num1 = int(input("\nDigite o primeiro número: "))
   num2 = int(input("\nDigite o segundo número: "))
